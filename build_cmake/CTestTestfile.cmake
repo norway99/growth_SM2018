a7c5e444d16296d85100a3169515e758a4e5b7c9
Aparna NK <aparna.nair-kanneganti@yale.edu>
2020-10-14 12:01:53 -0400

    remove compiled directory

--- build_cmake/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aparnank/mvlab/growth_SM2018_forked
-# Build directory: /home/aparnank/mvlab/growth_SM2018_forked/build_cmake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "/home/aparnank/mvlab/growth_SM2018_forked/bin/testshell")
-subdirs("googletest-build")
